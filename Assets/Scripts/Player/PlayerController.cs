a4098eb5cff1d121b81b5e180d1c27a8cc2297fe
Kammet07 <pavel.pupentsov@gmail.com>
2020-10-08 22:08:04 +0200

    Initial commit

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+
+    public Rigidbody2D rb;
+
+    private Vector2 _movement;
+
+    private void Update()
+    {
+        // Handles movement inputs
+        _movement.x = Input.GetAxisRaw("Horizontal");
+        _movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + _movement * moveSpeed * Time.fixedDeltaTime); // Moves the player
+    }
+}
